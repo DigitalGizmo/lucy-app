6787a4910fd19b7d424e83ba19d2c00d916df980
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-11-01 17:48:39 -0400

    links to total, fix mixed

--- src/routes/moments/[slug]/+page.svelte
+++ src/routes/moments/[slug]/+page.svelte
@@ -137,7 +137,7 @@
               <image transition:fade={{ duration: 500}} 
               class="image-layer" width="100%" height="100%" 
               transform="translate({leavesTransX} {leavesTransY})"
-              href="http://lucy-proto.deerfield-ma.org/assets/moments/images/community/leaves-only.png" />
+              href="https://lucy-proto.deerfield-ma.org/assets/moments/images/community/leaves-only.png" />
           {/if}
 
 
@@ -245,8 +245,12 @@
     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
     <h3>Who Else?</h3>
     <ul>
-        <li>This person</li>
-        <li>That person</li>
+        <li><a href="/"
+          on:click={(e) => { e.preventDefault(); showModal("Lucy Terry", "who");}}>Lucy Terry</a>
+        </li>
+        <li><a href="/"
+          on:click={(e) => { e.preventDefault(); showModal("Cesar", "who");}}>Cesar</a>
+        </li>
         <li>Another person</li>
         <li>A fourth person</li>
     </ul>
