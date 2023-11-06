86b9671e6658b89d151a3661f2ebd1aca1d100da
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-11-06 16:04:48 -0500

    added style wrapper to normalize

--- src/routes/moments/temptitle/+page.svelte
+++ src/routes/moments/temptitle/+page.svelte
@@ -3,27 +3,30 @@
   import MomentNav from "../../../lib/MomentNav.svelte";
 </script>
 
-<header id="header" class="moment-header">
-  <MainNav />
-  <MomentNav />
-</header>
+<section class="style-wrapper-tbd">
+  <header id="header" class="moment-header">
+    <MainNav />
+    <MomentNav />
+  </header>
+  
+  <section class="moment-title title-community">
+  
+    <h1>Community Within Community</h1>
+  
+    <article class="moment-title-intro">
+      <p class="story">Lucy and Abijah walk down main street. This is the story intro.</p>
+      <p class="history">This is the historical intro</p>
+    </article>
+  
+    <nav class="moment-options">
+      <ul>
+        <li class="prev-moment"><a href="/">Previous moment</a></li>
+        <li class="this-moment"><a href="/moments/community">Explore this moment</a></li>
+        <li class="next-moment"><a href="/">Next moment</a></li>
+      </ul>
+    </nav>
+  
+  </section><!--/moment-title-->
 
-<section class="moment-title title-community">
-
-  <h1>Community Within Community</h1>
-
-  <article class="moment-title-intro">
-    <p class="story">Lucy and Abijah walk down main street. This is the story intro.</p>
-    <p class="history">This is the historical intro</p>
-  </article>
-
-  <nav class="moment-options">
-    <ul>
-      <li class="prev-moment"><a href="/">Previous moment</a></li>
-      <li class="this-moment"><a href="/moments/community">Explore this moment</a></li>
-      <li class="next-moment"><a href="/">Next moment</a></li>
-    </ul>
-  </nav>
-
-</section><!--/moment-title-->
+</section>
 
