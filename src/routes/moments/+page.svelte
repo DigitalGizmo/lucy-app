e60dcf036213e518e04300dd06af1565a902cae7
Juliet Jacobson <julietanna@users.noreply.github.com>
2023-11-09 16:42:04 -0500

    added some placeholder content

--- src/routes/moments/+page.svelte
+++ src/routes/moments/+page.svelte
@@ -11,22 +11,21 @@
 	</header>
 
 	<section class="wrapper">
-		<p class="breadcrumbs">Moments (breadcrumb)</p>
 
         <ul>
-            <li><a href="/">Sold Out of Africa</a></li>
-            <li><a href="/">For Sale Again</a></li>
-            <li><a href="/">A Strange New Life</a></li>
-            <li><a href="/">Enslaved at the Wells’ House</a></li>
-            <li><a href="/">Joining the Church</a></li>
-            <li><a href="/">Singer of History</a></li>
+            <li><a href="/moments/sold">Sold Out of Africa</a></li>
+            <li><a href="/moments/forsale">For Sale Again</a></li>
+            <li><a href="/moments/newlife">A Strange New Life</a></li>
+            <li><a href="/moments/wells">Enslaved at the Wells’ House</a></li>
+            <li><a href="/moments/church">Joining the Church</a></li>
+            <li><a href="/moments/singer">Singer of History</a></li>
             <li><a href="/moments/engaging">Engaging With the Community</a></li>
             <li><a href="/moments/community">Community Within Community</a></li>
             <li><a href="/moments/union">A Fruitful Union</a></li>
-            <li><a href="/">Sons of the Revolution</a></li>
-            <li><a href="/">Living on the Frontier</a></li>
-            <li><a href="/">Fighting in Court</a></li>
-            <li><a href="/">Returning to Deerfield</a></li>
+            <li><a href="/moments/revolution">Sons of the Revolution</a></li>
+            <li><a href="/moments/frontier">Living on the Frontier</a></li>
+            <li><a href="/moments/court">Fighting in Court</a></li>
+            <li><a href="/moments/returning">Returning to Deerfield</a></li>
         </ul>  
 
 	</section><!-- /wrapper -->
