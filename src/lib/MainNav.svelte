d242948c9c1e55ae84440f4c1d1fd15f052d4cca
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-11-21 10:55:23 -0500

    a few comments on hamburger

--- src/lib/MainNav.svelte
+++ src/lib/MainNav.svelte
@@ -14,6 +14,12 @@
 
 <h3><a href="/">Lucy Terry Prince</a></h3>
 <nav class="global-nav" id="myTopnav">
+    <a href="/" 
+        class="hamburger" 
+        on:click={(e) => { e.preventDefault(); toggleHamburger();}}
+        >
+        <i class="fa fa-bars"></i>
+    </a>
     <ul>
         <li>
             <a href="/moments/">Moments</a>
@@ -27,11 +33,5 @@
         <li><a href="/maps/">Where in the World?</a></li>
         <li><a href="/myths/">Myths &amp; Assumptions</a></li>
     </ul>
-    <a href="/" 
-        class="hamburger" 
-        on:click={(e) => { e.preventDefault(); toggleHamburger();}}
-        >
-        <i class="fa fa-bars"></i>
-    </a>
 </nav>
   
