9bc86b76db196702ea1093fb214a4a4b89754ba7
Juliet Jacobson <julietanna@users.noreply.github.com>
2023-11-21 14:14:33 -0500

    repositioned, refined mobile main nav

--- src/lib/MainNav.svelte
+++ src/lib/MainNav.svelte
@@ -2,7 +2,7 @@
     import { page } from '$app/stores';
     function toggleHamburger() {
         console.log("hamburger")
-        const x = document.getElementById("myTopnav");
+        const x = document.getElementById("mainNav");
         if (x.className === "global-nav") {
             x.className += " responsive";
         } else {
@@ -13,7 +13,8 @@
 </script>
 
 <h3><a href="/">Lucy Terry Prince</a></h3>
-<nav class="global-nav" id="myTopnav">
+
+<nav class="global-nav" id="mainNav">
     <a href="/" 
         class="hamburger" 
         on:click={(e) => { e.preventDefault(); toggleHamburger();}}
@@ -21,13 +22,9 @@
         <i class="fa fa-bars"></i>
     </a>
     <ul>
-        <li>
-            <a href="/moments/">Moments</a>
-        </li>
+        <li><a href="/moments/">Moments</a></li>
         <!-- <li class="{ $page.path === '/people/' ? 'selected' : ''}"> -->
-        <li>
-        <a href="/people/">Who Else?</a>
-        </li>
+        <li><a href="/people/">Who Else?</a></li>
         <li><a href="/evidence/">How Do We Know?</a></li>
         <li><a href="/topics/">Topics &amp; Ideas</a></li>
         <li><a href="/maps/">Where in the World?</a></li>
