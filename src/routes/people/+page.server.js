a07446773579cdfbe73d9c6e88ae88eba8bc5354
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2023-12-07 11:50:29 -0500

    page to server.js

--- src/routes/people/+page.server.js
+++ src/routes/people/+page.server.js
@@ -1,4 +1,4 @@
-/** @type {import('./$types').PageLoad} */
+/// @type {import('./$types').PageLoad} 
 export async function load({ fetch }) {
     const res = await fetch('https://lucy-proto.deerfield-ma.org/people/persons/');
     const people = await res.json();
--- src/routes/people/[slug]/+page.server.js
+++ src/routes/people/[slug]/+page.server.js
@@ -1,4 +1,4 @@
-/** @type {import('./$types').PageLoad} */
+// @type {import('./$types').PageLoad} 
 
 export async function load({ fetch, params }) {
     const res = await fetch(`https://lucy-proto.deerfield-ma.org/people/persons/${params.slug}`);
