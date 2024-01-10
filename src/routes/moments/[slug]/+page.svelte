e310814aa6f5609d46c98670513dcfc9fb6be2a0
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2024-01-10 15:32:15 -0500

    nav links change image, text scroll unhinged

--- src/routes/moments/[slug]/+page.svelte
+++ src/routes/moments/[slug]/+page.svelte
@@ -385,57 +385,108 @@
 
   {:else}  <!-- title mode -->
     <section class="moment-title">
-      <!-- transition:slide={{ axis: 'x'}} -->
-      <!-- <div class="image-panel-fixed"> 
-        This was declared as: .image-panel div.image-panel-fixed
-        but it isn't/wasn't inside of .image-panel so was not valid
-      -->
-      <div >
 
+      <div class="image-panel-fixed">
         <div class="image-panel-image">
-          <img 
-            src="https://lucy-proto.deerfield-ma.org/assets/moments/images/titlescreens/{data.moment.slug}.jpg" 
-            alt="intro sketch">
-        </div><!-- /image-panel-image -->
-
-        <div class="title-container">
-          <h1>{data.moment.title}</h1>
-  
-          <article class="moment-title-intro">
-            <p class="story">{data.moment.storyIntro}</p>
-            <p class="history">{data.moment.historyIntro}</p>
-          </article>
-  
-          <nav class="moment-options">
-            <ul>
-              {#if getPrevSlugIdx(data.moment.slug) >= 0 }
-                <li class="prev-moment">
-                  <a href="/moments/{momentSlugs[getPrevSlugIdx(data.moment.slug)]}">
-                    &larr; Previous moment
-                  </a>
-                </li>
-              {/if}
-              {#if (data.moment.slug === "community")}
-                <li class="this-moment">
-                  <a href="/"
-                    on:click={(e) => { e.preventDefault(); explore();}}>
-                    Explore this moment &darr;
-                  </a>
-                </li>
-              {/if}
-              
-              {#if getNextSlugIdx(data.moment.slug) <= 12 }
-                <li class="next-moment">
-                  <a href="/moments/{momentSlugs[getNextSlugIdx(data.moment.slug)]}">
-                    Next moment &rarr;
-                  </a>
-                </li>
-              {/if}
-            </ul>
-          </nav>
-
+          <img
+            src="https://lucy-proto.deerfield-ma.org/assets/moments/images/titlescreens/{data.moment.slug}.jpg"
+            alt="intro sketch"
+          />
         </div>
-      </div> <!--/image panel fixed-->
+
+        <nav class="moment-options">
+          <ul>
+            {#if getPrevSlugIdx(data.moment.slug) >= 0 }
+              <li class="prev-moment">
+                <a href="/moments/{momentSlugs[getPrevSlugIdx(data.moment.slug)]}">
+                  &larr; Previous moment
+                </a>
+              </li>
+            {/if}
+            {#if (data.moment.slug === "community")}
+              <li class="this-moment">
+                <a href="/"
+                  on:click={(e) => { e.preventDefault(); explore();}}>
+                  Explore this moment &darr;
+                </a>
+              </li>
+            {/if}
+            
+            {#if getNextSlugIdx(data.moment.slug) <= 12 }
+              <li class="next-moment">
+                <a href="/moments/{momentSlugs[getNextSlugIdx(data.moment.slug)]}">
+                  Next moment &rarr;
+                </a>
+              </li>
+            {/if}
+          </ul>
+        </nav>
+
+      </div><!-- end image-panel-fixed -->   
+    
+      <div class="title-container">
+        <div class="horizontal-titles">
+    
+          <div class="horizontal-title">
+            <article>
+              <h1>Sold Out of Africa</h1>
+              <p class="story">
+                Lucy with many other Africans are captured and taken away from all
+                that is safe and familiar. Once she arrives at the coast she is
+                imprisoned, sold, and taken onto a ship bound for a strange world
+                thousands of miles from home.
+              </p>
+              <p class="history">
+                An endless demand among European colonizers for the essential labor to
+                produce sugar and other lucrative commodities is destroying the lives
+                of millions of African men, women, and children. Their captors and
+                others eager to profit from the booming trade in human beings engage
+                in a brutal winnowing process. Imprisoned in slave castles and other
+                holding areas on the West Africa coast, survivors are forced into the
+                ships that will transport them thousands of miles to the Americas in
+                the infamous Middle Passage.
+              </p>
+            </article>
+          </div><!-- /horizontal-title -->
+    
+          <div class="horizontal-title">
+            <article>
+              <h1>For Sale Again</h1>
+              <p class="story">
+                Three-year old Lucy is among a group of captured Africans for sale in
+                Bristol, Rhode Island.
+              </p>
+              <p class="history">
+                Africans who survive capture and the horrors of the Middle Passage
+                encounter a new world where the loss of autonomy and the arbitrary
+                power of their enslavers are the only constants.
+              </p>
+            </article>
+          </div><!-- /horizontal-title -->
+    
+          <div class="horizontal-title">
+            <article>
+              <h1>A Strange New Life</h1>
+              <p class="story">
+                Eight-year-old Lucy arrives in Deerfield and meets the couple who are
+                purchasing her from Samuel Terry. Once again, her life is about to
+                change but her status as an enslaved child does not.
+              </p>
+              <p class="history">
+                Enslaved people often experienced multiple captivities. They could be
+                sold at a moment’s notice to new captors anytime, anywhere, upending
+                in the process any relationships they may have been able to establish,
+                including marriage, children and siblings. Dealing with the loss of
+                old connections and forging new ones required resilience, courage, and
+                persistence by young and old alike.
+              </p>
+            </article>
+          </div><!-- /horizontal-title -->
+    
+        </div><!-- /horizontal-titles -->
+      </div><!-- /title-container -->
+    
+
 
     </section><!--/moment-title-->
 
