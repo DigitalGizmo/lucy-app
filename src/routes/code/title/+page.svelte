239acaa03bf948a5bc60cc31964a7854e4896be2
Juliet Jacobson <julietanna@users.noreply.github.com>
2024-01-16 10:21:59 -0500

    baby step #1

--- src/routes/code/title/+page.svelte
+++ src/routes/code/title/+page.svelte
@@ -67,63 +67,30 @@
         <div id="horizontal-titles"
           bind:this={horizontalTitles}
           on:scroll={(e)=> console.log('left :' + e.target.scrollLeft)} >
-    
-          <div class="horizontal-title">
-            <article>
-              <h1>Sold Out of Africa</h1>
-              <p class="story"> 
-                debug: {sLeft}
-                Lucy with many other Africans are captured and taken away from all
-                that is safe and familiar. Once she arrives at the coast she is
-                imprisoned, sold, and taken onto a ship bound for a strange world
-                thousands of miles from home. 
-              </p>
-              <p class="history">
-                An endless demand among European colonizers for the essential labor to
-                produce sugar and other lucrative commodities is destroying the lives
-                of millions of African men, women, and children. Their captors and
-                others eager to profit from the booming trade in human beings engage
-                in a brutal winnowing process. Imprisoned in slave castles and other
-                holding areas on the West Africa coast, survivors are forced into the
-                ships that will transport them thousands of miles to the Americas in
-                the infamous Middle Passage.
-              </p>
-            </article>
-          </div><!-- /horizontal-title -->
-    
-          <div class="horizontal-title">
-            <article>
-              <h1>For Sale Again</h1>
-              <p class="story">
+             
+              <p>
                 Three-year old Lucy is among a group of captured Africans for sale in
                 Bristol, Rhode Island.
               </p>
-              <p class="history">
+              <p>
                 Africans who survive capture and the horrors of the Middle Passage
                 encounter a new world where the loss of autonomy and the arbitrary
                 power of their enslavers are the only constants.
               </p>
-            </article>
-          </div><!-- /horizontal-title -->
-    
-          <div class="horizontal-title">
-            <article>
-              <h1>A Strange New Life</h1>
-              <p class="story">
+              <p>
                 Eight-year-old Lucy arrives in Deerfield and meets the couple who are
                 purchasing her from Samuel Terry. Once again, her life is about to
                 change but her status as an enslaved child does not.
               </p>
-              <p class="history">
+              <p>
                 Enslaved people often experienced multiple captivities. They could be
                 sold at a moment’s notice to new captors anytime, anywhere, upending
                 in the process any relationships they may have been able to establish,
                 including marriage, children and siblings. Dealing with the loss of
                 old connections and forging new ones required resilience, courage, and
                 persistence by young and old alike.
-              </p>
-            </article>
-          </div><!-- /horizontal-title -->
+              </p>    
+
     
         </div><!-- /horizontal-titles -->
       </div><!-- /title-container -->
