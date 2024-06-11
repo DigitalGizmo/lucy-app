8b1d95ee071894ee007173925d9a2181805caeb2
Juliet Jacobson <julietanna@users.noreply.github.com>
2024-06-11 11:09:56 -0400

    tweaks to Moments Menu

--- src/routes/when/+page.svelte
+++ src/routes/when/+page.svelte
@@ -17,38 +17,38 @@
     
   <div class="feature-menu maps">
       <ul class="col-heads">
-        <li class="feature-title">Date/Title 1</li>
+        <li class="feature-title">Date/Title</li>
         <li class="feature-blurb">About</li>
       </ul>
 
       <ul class="col-content">
-        <li class="feature-title">Slave Ship Leusden Sinks, January 1, 1738
-The Leusden sunk off the coast of Suriname, a Dutch colony. Over 600 enslaved Africans drowned in the largest marine tragedy of the Trans-Atlantic slave trade. Captain and crew survived, having left almost 700 imprisoned Africans intentionally trapped in the ship.</li>
+        <li class="feature-title">1738<br>Slave Ship Leusden Sinks, January 1, 1738</li>
+        <li>The Leusden sunk off the coast of Suriname, a Dutch colony. Over 600 enslaved Africans drowned in the largest marine tragedy of the Trans-Atlantic slave trade. Captain and crew survived, having left almost 700 imprisoned Africans intentionally trapped in the ship.</li>
       </ul>
 
       <ul class="col-content">
-        <li class="feature-title">Moment: Enslaved at the Wells’s House, 1740</li>
+        <li class="feature-title">1740<br>Moment: Enslaved at the Wells’s House</li>
         <li class="feature-blurb">Lucy and Cesar are expected to undertake the most menial tasks on a rural farm in an unending cycle of labor.</li>
       </ul>
 
       <ul class="col-content">
-        <li class="feature-title">How Do We Know?: Great Wheel, c. 1740</li>
+        <li class="feature-title">c.1740<br>How Do We Know?: Great Wheel</li>
         <li class="feature-blurb">In the 18th and early 19th centuries a spinning wheel was a common feature in most homes, as part of most women’s household chores included spinning yarn.</li>
       </ul>
 
       <ul class="col-content">
-        <li class="feature-title">Other Lives: Heber Honestman’s Homestead, 1740</li>
+        <li class="feature-title">1740<br>Other Lives: Heber Honestman’s Homestead</li>
         <li class="feature-blurb">Heber Honestman purchased land in newly established Huntstown (now Ashfield), a tiny community in the then-unsettled hill region of western Massachusetts. Heber and Susanna, his wife drew Lot #1 having purchased the right to participate from “Josiah Prat in his Father’s Right viz Samll”.</li>
       </ul>
 
       <ul class="col-content">
-        <li class="feature-title">Other Lives: Venture Smith, 1741 - 1797</li>
-        <li class="feature-blurb">Venture Smith recalls his life in Africa, his capture, and enslavement in Connecticut. He purchased not only his own freedom but that of his family and three African American men.</li>
+        <li class="feature-title">1741<br>How Do We Know?: Boston Law Restricting Movement of Blacks</li>
+        <li class="feature-blurb">Lorem ipsum dolor sit amet consectetur. Suscipit scelerisque scelerisque elit sagittis massa placerat eleifend. Quam vel amet aliquam cras convallis dis arcu at nullam.</li>
       </ul>
 
       <ul class="col-content">
-        <li class="feature-title">How Do We Know?: Boston Law Restricting Movement of Blacks, 1741</li>
-        <li class="feature-blurb">Lorem ipsum dolor sit amet consectetur. Suscipit scelerisque scelerisque elit sagittis massa placerat eleifend. Quam vel amet aliquam cras convallis dis arcu at nullam.</li>
+        <li class="feature-title">1744 - 1829<br>Other Lives: Elizabeth Freeman (Mumbet)</li>
+        <li class="feature-blurb">In 1781, Elizabeth "Mumbet" Freeman of Sheffield, Massachusetts, fought for and won freedom for herself and a man named Brom.</li>
       </ul>
 
     </div>
