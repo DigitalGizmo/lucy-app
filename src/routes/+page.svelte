cadeb463555b406526396d2cda72c430ebca121e
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2026-04-22 10:55:49 -0400

    add credits from the proto version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,7 @@
             <li><a data-sveltekit-reload href="/topics/">Topics &amp; Ideas</a></li>
             <li><a data-sveltekit-reload href="/maps/">Where in the World?</a></li>
             <li><a data-sveltekit-reload href="/">When?</a></li>
+            <li><a data-sveltekit-reload href="/credits/">Credits</a></li>
         </ul>
 
         <p class="wireframe-note">WIREFRAME ONLY - NOT YET DESIGNED<br>
